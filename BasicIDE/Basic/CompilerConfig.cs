286a5731cd5123bdcfc694cef5f5019ce95a030f
Kevin Gut <kevin.gut@bitm.sg>
2022-05-16 04:18:57 +0200

    Add comments to compiler config

--- BasicIDE/Basic/CompilerConfig.cs
+++ BasicIDE/Basic/CompilerConfig.cs
@@ -1,19 +1,44 @@
 ﻿namespace BasicIDE.Basic
 {
+    /// <summary>
+    /// Controls behavior of the compiler
+    /// </summary>
     public class CompilerConfig
     {
+        /// <summary>
+        /// Gets or sets the number to begin numbering BASIC lines at
+        /// </summary>
         public int StartLine { get; set; }
 
+        /// <summary>
+        /// Gets or sets the line number increment for BASIC lines
+        /// </summary>
         public int LineIncrement { get; set; }
 
+        /// <summary>
+        /// Gets or sets the variable used to handle function returns
+        /// </summary>
         public string ReturnVar { get; set; }
 
+        /// <summary>
+        /// Gets or sets whether to strip comments
+        /// </summary>
+        /// <remarks>Only commands entirely consisting of a comment will be stripped</remarks>
         public bool StripComments { get; set; }
 
+        /// <summary>
+        /// Gets or sets whether to strip debug instructions
+        /// </summary>
         public bool StripDebug { get; set; }
 
+        /// <summary>
+        /// Gets or sets whether to treat warnings as errors
+        /// </summary>
         public bool TreatWarningsAsErrors { get; set; }
 
+        /// <summary>
+        /// Creates a default instance
+        /// </summary>
         public CompilerConfig()
         {
             StartLine = 10;
@@ -24,6 +49,9 @@
             TreatWarningsAsErrors = false;
         }
 
+        /// <summary>
+        /// Gets a configuration suitable for release mode builds
+        /// </summary>
         public static CompilerConfig Release
         {
             get
@@ -32,11 +60,16 @@
                 {
                     LineIncrement = 1,
                     StartLine = 1,
-                    StripDebug = true
+                    StripDebug = true,
+                    StripComments = true,
+                    TreatWarningsAsErrors = true
                 };
             }
         }
 
+        /// <summary>
+        /// Gets a configuration suitable for debug mode builds
+        /// </summary>
         public static CompilerConfig Debug
         {
             get
@@ -45,7 +78,9 @@
                 {
                     LineIncrement = 10,
                     StartLine = 10,
-                    StripComments = false
+                    StripComments = false,
+                    StripDebug = false,
+                    TreatWarningsAsErrors = false
                 };
             }
         }
