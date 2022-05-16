d3b307c6f5effb800bc18eed93ae45f05735f2a9
Kevin Gut <kevin.gut@bitm.sg>
2022-05-16 04:29:19 +0200

    Add comments to SyntaxError

--- BasicIDE/Basic/SyntaxError.cs
+++ BasicIDE/Basic/SyntaxError.cs
@@ -2,16 +2,38 @@
 
 namespace BasicIDE.Basic
 {
+    /// <summary>
+    /// Represents a syntax message
+    /// </summary>
     public class SyntaxError : IValidateable
     {
+        /// <summary>
+        /// Gets the source index of the line
+        /// </summary>
         public int LineIndex { get; } = -1;
 
+        /// <summary>
+        /// Gets the type of message
+        /// </summary>
         public SyntaxErrorType ErrorType { get; }
 
+        /// <summary>
+        /// Gets the message string
+        /// </summary>
         public string Message { get; }
 
+        /// <summary>
+        /// Gets the current function name
+        /// </summary>
         public string FunctionName { get; }
 
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="Line">Line index</param>
+        /// <param name="Type">Message type</param>
+        /// <param name="Message">Message string</param>
+        /// <param name="FunctionName">Current function name</param>
         public SyntaxError(int Line, SyntaxErrorType Type, string Message = null, string FunctionName = null)
         {
             if (Line < 0)
@@ -25,6 +47,12 @@ namespace BasicIDE.Basic
             Validate();
         }
 
+        /// <summary>
+        /// Creates a new instance for when no line index is present
+        /// </summary>
+        /// <param name="Type">Message type</param>
+        /// <param name="Message">Message string</param>
+        /// <param name="FunctionName">Current function name</param>
         public SyntaxError(SyntaxErrorType Type, string Message = null, string FunctionName = null)
         {
             ErrorType = Type;
@@ -33,6 +61,9 @@ namespace BasicIDE.Basic
             Validate();
         }
 
+        /// <summary>
+        /// Validates this instance
+        /// </summary>
         public void Validate()
         {
             if (!Enum.IsDefined(typeof(SyntaxErrorType), ErrorType))
@@ -41,16 +72,35 @@ namespace BasicIDE.Basic
             }
         }
 
+        /// <summary>
+        /// Gets a display string
+        /// </summary>
+        /// <returns>Display string</returns>
         public override string ToString()
         {
             return $"Line {LineIndex + 1}: [{ErrorType}] {Message}";
         }
     }
 
+    /// <summary>
+    /// Possible syntax error message types
+    /// </summary>
     public enum SyntaxErrorType
     {
+        /// <summary>
+        /// Informational message
+        /// </summary>
+        /// <remarks>These are usually improvements the programmer could make</remarks>
         Info = 0,
+        /// <summary>
+        /// Warning message
+        /// </summary>
+        /// <remarks>Highlights potential mistakes</remarks>
         Warning = 1,
+        /// <summary>
+        /// Error message
+        /// </summary>
+        /// <remarks>Problems that prevent compilation</remarks>
         Error = 2
     }
 }
