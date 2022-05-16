429bffe13a489b40c6d65da1c9c3041bc4b05948
Kevin Gut <kevin.gut@bitm.sg>
2022-05-16 04:22:42 +0200

    Add comments to function declaration

--- BasicIDE/Basic/FunctionDeclaration.cs
+++ BasicIDE/Basic/FunctionDeclaration.cs
@@ -7,14 +7,31 @@ using System.Threading.Tasks;
 
 namespace BasicIDE.Basic
 {
+    /// <summary>
+    /// Represents a BASIC function declaration
+    /// </summary>
     public class FunctionDeclaration
     {
+        /// <summary>
+        /// List of function arguments
+        /// </summary>
         private readonly List<string> args;
 
+        /// <summary>
+        /// Gets the name of the function
+        /// </summary>
         public string FunctionName { get; }
 
+        /// <summary>
+        /// Gets function arguments
+        /// </summary>
         public string[] Arguments => args.ToArray();
 
+        /// <summary>
+        /// Creates a new instance
+        /// </summary>
+        /// <param name="FunctionName">Function name</param>
+        /// <param name="Lines">Function code</param>
         public FunctionDeclaration(string FunctionName, string[] Lines)
         {
             if (string.IsNullOrWhiteSpace(FunctionName))
@@ -42,9 +59,13 @@ namespace BasicIDE.Basic
             }
         }
 
+        /// <summary>
+        /// Returns a display string in the format "func(arg, arg, ...)"
+        /// </summary>
+        /// <returns>Display string</returns>
         public override string ToString()
         {
-            var Params = string.Join(",", args);
+            var Params = string.Join(", ", args);
             return $"{FunctionName}({Params})";
         }
     }
